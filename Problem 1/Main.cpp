7cb17c15c0a080dafa26e2f5c909fc2f25f18cfd
TheOneSpunky <kyle.swecker@gmail.com>
2023-04-27 00:40:34 -0500

    Generalized the solution to problem 1, and optimized for larger inputs.

--- Problem 1/Main.cpp
+++ Problem 1/Main.cpp
@@ -7,21 +7,63 @@
  */
 
 #include <iostream>
+#include <vector>
+#include <bitset>
+#include <numeric>
+#include <future>
+#include <mutex>
+
+std::mutex g_mtx;
+
+auto sumMultiplesPartial(int start, int end, const std::vector<int>& terms, int limit, int* sum) -> void {
+  const auto numTerms{ static_cast<int>(terms.size()) };
+
+  for (auto i{ start }; i < end; i++) {
+    auto bits { std::bitset<32>{static_cast<unsigned __int64>(i)} };
+    auto lcm  { 1 };
+    auto sign { bits.count() % 2 == 0 ? -1 : 1 };
+
+    for (auto j{ 0 }; j < numTerms; j++)
+      if (bits[j])
+        lcm = std::lcm(lcm, terms[j]);
+
+    auto n          { (limit - 1) / lcm };
+    auto partialSum { sign * lcm * n * (n + 1) / 2 };
+    
+    auto lock{ std::unique_lock<std::mutex>{g_mtx} };
+    *sum += partialSum;
+    lock.unlock();
+  }
+}
+
+auto sumMultiples(const std::vector<int>& terms, int limit) -> int {
+  auto       sum             { 0 };
+  const auto numTerms        { static_cast<int>(terms.size()) };
+  const auto numCombinations { 1 << numTerms };
+
+  const auto numThreads { static_cast<int>(std::thread::hardware_concurrency()) };
+  auto       futures    { std::vector<std::future<void>>{} };
+
+  const auto range{ numCombinations / numThreads };
+
+  for (auto i{ 0 }; i < numThreads; i++) {
+    const auto start { i * range + 1 };
+    const auto end   { (i == numThreads - 1) ? numCombinations : (i + 1) * range };
+
+    futures.push_back(std::async(std::launch::async, sumMultiplesPartial, start, end, std::ref(terms), limit, &sum));
+  }
+
+  for (const auto& future : futures)
+    future.wait();
+
+  return sum;
+}
 
 auto main() -> int {
   constexpr auto limit { 1000 };
-  constexpr auto a     { 3 };
-  constexpr auto b     { 5 };
-  constexpr auto c     { a * b };
+  const auto     terms { std::vector<int>{3, 5} };
 
-  const auto sumMultiples {
-    [&limit](const int& factor) -> int {
-      const auto n{ (limit - 1) / factor };
-      return (factor * n * (n + 1) / 2);
-    }
-  };
-
-  const auto sum{ sumMultiples(a) + sumMultiples(b) - sumMultiples(c) };
+  const auto sum{ sumMultiples(terms, limit) };
 
   std::cout << sum << std::endl;
 
