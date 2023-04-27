99cdb0908bb24bd4216b6cf640ac471e7bd1866a
TheOneSpunky <kyle.swecker@gmail.com>
2023-04-27 06:31:45 -0500

    Further optmize solution to problem 3.

--- Problem 3/Main.cpp
+++ Problem 3/Main.cpp
@@ -9,37 +9,23 @@
 #include <iostream>
 #include <cmath>
 
-auto isPrime(long long n) -> bool {
-  if (n <= 1)
-    return false;
-  else if (n == 2 || n == 3)
-    return true;
-  else if (n % 2 == 0)
-    return false;
+auto largestPrimeFactor(long long number) -> long long {
+  while (number % 2 == 0)
+    number /= 2;
 
-  const auto limit{ static_cast<long long>(std::sqrt(n)) + 1 };
+  if (number == 1)
+    return 2;
 
-  for (auto i{ long long{3} }; i < limit; i += 2)
-    if (n % i == 0)
-      return false;
+  for (auto i{ long long{3} }; i <= std::sqrt(number); i += 2)
+    while (number % i == 0)
+      number /= i;
 
-  return true;
+  return ((number > 1) ? number : 2);
 }
 
 auto main() -> int {
-  constexpr auto number{ long long{600851475143} };
-
-  auto largestFactor{ long long{1} };
-
-  for (auto i{ long long{2} }; i <= sqrt(number); i++)
-    if (number % i == 0) {
-      auto otherFactor{ long long{number / i} };
-
-      if (isPrime(i))
-        largestFactor = i;
-      else if (isPrime(otherFactor))
-        largestFactor = otherFactor;
-    }
+  constexpr auto number        { long long{600851475143} };
+  auto           largestFactor { largestPrimeFactor(number) };
 
   std::cout << largestFactor << std::endl;
 
