2269e8b9c4c89e2d0ba0feae22416383b44e588d
TheOneSpunky <kyle.swecker@gmail.com>
2023-04-30 05:11:23 -0500

    Add solution to problem 10.

---
+++ Problem 10/Main.cpp
@@ -0,0 +1,44 @@
+/**
+ * Project Euler Problem 10: Summation of primes
+ * 
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ */
+
+#include <iostream>
+#include <vector>
+#include <numeric>
+
+auto sieveOfEratosthenes(int limit) -> std::vector<bool> {
+  auto primes{ std::vector<bool>(limit + 1, true) };
+
+  primes[0] = primes[1] = false;
+
+  for (auto i{ 2 }; i * i <= limit; i++)
+    if (primes[i])
+      for (auto j{ i * i }; j <= limit; j += i)
+        primes[j] = false;
+
+  return primes;
+}
+
+auto sumPrimes(int limit) -> long long {
+  auto primes { sieveOfEratosthenes(limit) };
+  auto sum    { long long{0} };
+
+  for (auto i{ 2 }; i < limit; i++)
+    if (primes[i])
+      sum += i;
+
+  return sum;
+}
+
+auto main() -> int {
+  const auto limit { 2000000 };
+  const auto sum   { sumPrimes(limit) };
+
+  std::cout << sum << std::endl;
+
+  return 0;
+}
