c2d2c7b77282f951e94a4fe59a43fe143a3cbfee
TheOneSpunky <kyle.swecker@gmail.com>
2023-05-01 09:15:56 -0500

    Optimize solution to problem 16 by directly calculating the multiplication of large numbers.

--- Problem 16/Main.cpp
+++ Problem 16/Main.cpp
@@ -11,47 +11,25 @@
 #include <vector>
 #include <algorithm>
 
-auto add(const std::string& a, const std::string& b) -> std::string {
-  const auto aLength{ static_cast<int>(a.size()) };
-  const auto bLength{ static_cast<int>(b.size()) };
+auto multiplyByTwo(const std::string& num) -> std::string {
+  const auto numLength{ static_cast<int>(num.size()) };
 
-  auto result { std::string{} };
-  auto carry  { 0 };
-  auto sum    { 0 };
+  auto result  { std::string{} };
+  auto carry   { 0 };
+  auto product { 0 };
 
-  for (auto i{ 0 }; i < std::max(aLength, bLength); i++) {
-    sum = carry;
+  for (auto i{ numLength - 1 }; i >= 0; i--) {
+    product  = (num[i] - '0') * 2 + carry;
+    carry    = product / 10;
+    product %= 10;
 
-    if (i < aLength)
-      sum += a[aLength - 1 - i] - '0';
-    if (i < bLength)
-      sum += b[bLength - 1 - i] - '0';
-
-    carry  = sum / 10;
-    sum   %= 10;
-
-    result.insert(result.begin(), static_cast<char>(sum) + '0');
+    result.insert(result.begin(), static_cast<char>(product) + '0');
   }
 
   if (carry > 0)
     result.insert(result.begin(), static_cast<char>(carry) + '0');
 
   return result;
-}
-
-auto powerOfTwo(int exponent) -> std::string {
-  auto num  { std::string{"1"} };
-  auto base { std::string{"2"} };
-
-  while (exponent > 0) {
-    if (exponent % 2 == 1)
-      num = add(num, num);
-
-    base      = add(base, base);
-    exponent /= 2;
-  }
-
-  return num;
 }
 
 auto main() -> int {
@@ -61,7 +39,7 @@ auto main() -> int {
   auto num { std::string{"1"} };
 
   for (auto i{ 0 }; i < exponent; i++)
-    num = add(num, num);
+    num = multiplyByTwo(num);
 
   for (const auto& c : num)
     sum += c - '0';
