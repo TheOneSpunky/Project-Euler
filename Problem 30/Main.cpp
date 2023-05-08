2357b03061dd74d8e8e99d51d9dd4572e28c3982
TheOneSpunky <kyle.swecker@gmail.com>
2023-05-07 21:06:21 -0500

    Add solution to problem 30.

---
+++ Problem 30/Main.cpp
@@ -0,0 +1,50 @@
+/**
+ * Project Euler Problem 30: Digit fifth powers
+ * 
+ * Surprisingly there are only three numbers that can be written as the sum of fourth powers of their digits:
+ *
+ * 1634 = 1^4 + 6^4 + 3^4 + 4^4
+ * 8208 = 8^4 + 2^4 + 0^4 + 8^4
+ * 9474 = 9^4 + 4^4 + 7^4 + 4^4
+ * As 1 = 1^4 is not a sum it is not included.
+ *
+ * The sum of these numbers is 1634 + 8208 + 9474 = 19316.
+ *
+ * Find the sum of all the numbers that can be written as the sum of fifth powers of their digits.
+ */
+
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+auto isSumOfFifthPower(int number) -> bool {
+  const auto original { number };
+  auto       sum      { 0 };
+
+  while (number > 0) {
+    const auto digit{ number % 10 };
+
+    sum    += static_cast<int>(std::pow(digit, 5));
+    number /= 10;
+  }
+
+  return sum == original;
+}
+
+auto main() -> int {
+  const auto upper   { 6 * std::pow(9, 5) }; // 6 is the maximum number of digits we need to consider
+  auto       numbers { std::vector<int>{} };
+
+  for (auto i{ 10 }; i <= upper; i++)
+    if (isSumOfFifthPower(i))
+      numbers.push_back(i);
+
+  auto sum{ 0 };
+
+  for (int number : numbers)
+    sum += number;
+
+  std::cout << sum << std::endl;
+
+  return 0;
+}
