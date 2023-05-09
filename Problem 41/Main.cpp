b9025d6d3ce7b658f896b32ff9c7a69d706185ff
TheOneSpunky <kyle.swecker@gmail.com>
2023-05-09 07:31:08 -0500

    Optimize solution to problem 41.

--- Problem 41/Main.cpp
+++ Problem 41/Main.cpp
@@ -28,34 +28,11 @@ auto isPrime(const int& n) -> bool {
   return true;
 }
 
-auto isPandigital(int n) -> bool {
-  auto digits{ std::vector<int>{} };
-
-  while (n > 0) {
-    const auto digit{ n % 10 };
-
-    if (digit == 0)
-      return false;
-
-    digits.push_back(digit);
-
-    n /= 10;
-  }
-
-  std::sort(digits.begin(), digits.end());
-
-  for (auto i{ 0ULL }; i < digits.size(); i++)
-    if (digits[i] != i + 1)
-      return false;
-
-  return true;
-}
-
 auto main() -> int {
   constexpr auto maxn                   { 7 };
   auto           largestPandigitalPrime { 0 };
 
-  for (auto n{ 1 }; n <= maxn; n++) {
+  for (auto n{ maxn }; n > 0; n--) {
     auto digits{ std::vector<int>(n) };
 
     for (auto i{ 1 }; i <= n; i++)
@@ -67,9 +44,12 @@ auto main() -> int {
       for (const int& digit : digits)
         num = num * 10 + digit;
 
-      if (isPrime(num) && isPandigital(num))
+      if (isPrime(num))
         largestPandigitalPrime = std::max(largestPandigitalPrime, num);
     } while (std::next_permutation(digits.begin(), digits.end()));
+
+    if (largestPandigitalPrime > 0)
+      break;
   }
 
   std::cout << largestPandigitalPrime << std::endl;
