6fc59c2534a52ad1270b2ea29772d4de49fb4b4f
TheOneSpunky <kyle.swecker@gmail.com>
2023-05-09 01:18:47 -0500

    Optimize solution to problem 39 using Euclid's Formula.

--- Problem 39/Main.cpp
+++ Problem 39/Main.cpp
@@ -9,33 +9,40 @@
  */
 
 #include <iostream>
+#include <vector>
 
-auto isRightTriangle(const int& a, const int& b, const int& c) -> bool {
-  return (a * a + b * b == c * c);
+auto gcd(int a, int b) -> int {
+  while (b != 0) {
+    const auto t{ b };
+
+    b = a % b;
+    a = t;
+  }
+
+  return a;
 }
 
 auto main() -> int {
-  constexpr auto limit{ 1000 };
+  constexpr auto limit          { 1000 };
+  auto           solutionCounts { std::vector<int>(limit + 1, 0) };
+
+  for (auto m{ 2 }; m * m < limit; m++)
+    for (auto n{ 1 }; n < m; n++)
+      if (((m - n) % 2 == 1) && (gcd(m, n) == 1)) {
+        const auto p{ 2 * m * (m + n) };
+
+        for (auto k{ p }; k <= limit; k += p)
+          solutionCounts[k]++;
+      }
 
   auto maxSolutions { 0 };
   auto maxP         { 0 };
 
-  for (auto p{ 1 }; p <= limit; p++) {
-    auto solutions{ 0 };
-
-    for (auto a{ 1 }; a < p / 2; a++)
-      for (auto b{ a }; b < p / 2; b++) {
-        const auto c{ p - a - b };
-
-        if (isRightTriangle(a, b, c))
-          solutions++;
-      }
-
-    if (solutions > maxSolutions) {
-      maxSolutions = solutions;
+  for (auto p{ 1 }; p <= limit; p++)
+    if (solutionCounts[p] > maxSolutions) {
+      maxSolutions = solutionCounts[p];
       maxP         = p;
     }
-  }
 
   std::cout << maxP << std::endl;
 
