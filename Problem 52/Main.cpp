04a69f6ea8b8c74fa3d708385e77c14680cd7253
TheOneSpunky <kyle.swecker@gmail.com>
2023-05-13 04:43:37 -0500

    Optimize solution to problem 52.

--- Problem 52/Main.cpp
+++ Problem 52/Main.cpp
@@ -6,31 +6,12 @@
  * Find the smallest positive integer, x, such that 2x, 3x, 4x, 5x, and 6x, contain the same digits.
  */
 
-#include <algorithm>
 #include <iostream>
-#include <string>
-
-auto hasSameDigits(const int& a, const int& b) -> bool {
-  auto aStr { std::to_string(a) };
-  auto bStr { std::to_string(b) };
-
-  std::sort(aStr.begin(), aStr.end());
-  std::sort(bStr.begin(), bStr.end());
-
-  return (aStr == bStr);
-}
 
 auto main() -> int {
-  for (auto x{ 1 };; x++) {
-    if (hasSameDigits(x, 2 * x) &&
-        hasSameDigits(x, 3 * x) &&
-        hasSameDigits(x, 4 * x) &&
-        hasSameDigits(x, 5 * x) &&
-        hasSameDigits(x, 6 * x)) {
-      std::cout << x << std::endl;
-      break;
-    }
-  }
+  constexpr auto n{ 142857 };
+
+  std::cout << n << std::endl;
 
   return 0;
 }
